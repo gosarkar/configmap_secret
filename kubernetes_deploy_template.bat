c895d8516ac8bdb638db24dff8219bc92e04cf99
gosarkar <gosarkar@in.ibm.com>
2019-02-12 13:42:32 +0530

    Create kubernetes_deploy_template.bat

---
+++ kubernetes_deploy_template.bat
@@ -0,0 +1,10 @@
+SET KUBECONFIG=CLUSTER_KUBE_CONFIG_PATH
+kubectl apply -f CONFIG_MAP_YAML_FILE1
+kubectl apply -f CONFIG_MAP_YAML_FILE2
+kubectl apply -f SECRET_YAML_FILE
+kubectl delete configmap CONFIG_MAP_PROP
+kubectl create configmap CONFIG_MAP_PROP --from-file=CONFIG_MAP_PROP_FILE1
+kubectl delete -f SERVICE_YAML_FILE1 
+kubectl delete -f SERVICE_YAML_FILE2 
+kubectl create -f SERVICE_YAML_FILE1 
+kubectl create -f SERVICE_YAML_FILE2 
